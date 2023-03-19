3528f889a675eafa056bf45d167c41b4a0d83182
JeonghoPark <pjhpjh0805@naver.com>
2023-03-19 15:06:50 +0900

    test12

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -3,5 +3,7 @@ package com.example.demo.service;
 public class BoardService {
     public static void main(String[] args) {
         System.out.println("하이하이");
+
+        System.out.println("하이하이가 없어지나?");
     }
 }
